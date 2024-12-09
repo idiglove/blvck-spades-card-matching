bdd1c1a97a14bce97df52e82390faab8ca3cf7c2
Faith Morante <faith@gmail.com>
2024-12-08 20:09:12 -0800

    fix metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { CardType } from "@/types";
 
 export const metadata: Metadata = {
   title: "Blvck Spades Card Matching Game",
-  description: "Play our game to with discounts on our store",
+  description: "Play our game to win discounts on our store",
 };
 
 export default function Home() {
