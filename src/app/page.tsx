a162651b0a53f42cba08db196438c942beb4034a
Faith Morante <faith@gmail.com>
2024-12-08 20:05:03 -0800

    add some spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
 
   const shuffledCards = cards.sort(() => Math.random() - 0.5);
   return (
-    <main className="h-screen flex flex-col gap-8 row-start-2 items-center justify-center bg-white bg-custom bg-no-repeat bg-cover">
+    <main className="p-5 h-screen flex flex-col gap-8 row-start-2 items-center justify-center bg-white bg-custom bg-no-repeat bg-cover">
       <h1 className="text-3xl font-bold bg-gradient-to-t from-gold to-black-500 p-2 text-black">
         Blvck Spades Matching Game
       </h1>
